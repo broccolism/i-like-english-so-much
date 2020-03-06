29c49dea0fedd8600a93cde98f71df80135a93c8
broccoli <mile3880@gmail.com>
2020-03-07 07:44:01 +0900

    [fix] detect duplication

--- I Like English So Much.py
+++ I Like English So Much.py
@@ -1,4 +1,13 @@
 
+
+#####===============UI 및 기능
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+from enum import Enum
+
 #####===============파이어 베이스와 연동
 import firebase_admin
 from firebase_admin import credentials
@@ -11,12 +20,6 @@ cred = credentials.Certificate("D:\git_private\win_to_lin\i-like-english-so-much
 db_url = 'https://i-like-english-so-much.firebaseio.com/'
 voca_db = firebase_admin.initialize_app(cred, {'databaseURL' : db_url})
 
-#####===============UI 및 기능
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5 import uic
-
 #UI파일 연결
 #memo_class = uic.loadUiType("memorizing.ui")[0]
 main_class = uic.loadUiType("vocabulary.ui")[0]
@@ -28,9 +31,20 @@ class MemorizeClass(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)'''
 
+
+    
+class Category(Enum):
+    EIGHT = 0
+    NINE = 1
+    BASIC = 2
+    MAIN = 3
+    ELSE = 4
+
 #첫 화면 띄울 때 사용하는 기본 class 선언
 class MainClass(QMainWindow, main_class):
     def __init__(self):
+        global Category
+        
         super().__init__()
         self.title = '하하! 신나게 단어나 외우자고!'
         self.icon = self.style().standardIcon(getattr(QStyle, 'SP_FileDialogDetailedView'))
@@ -38,12 +52,18 @@ class MainClass(QMainWindow, main_class):
         self.setWindowTitle(self.title)
         self.setWindowIcon(self.icon)
 
+        self.arr = [[self.option1_800_button, self.option1_800_input_kor, self.option1_800_input_eng],
+               [self.option1_900_button, self.option1_900_input_kor, self.option1_900_input_eng],
+               [self.option1_basic_button, self.option1_basic_input_kor, self.option1_basic_input_eng],
+               [self.option1_main_button, self.option1_main_input_kor, self.option1_main_input_eng],
+               [self.option1_else_button, self.option1_else_input_kor, self.option1_else_input_eng]]
+
         #UI 객체에 연결
-        self.option1_800_button.clicked.connect(lambda:self.get_input("800"))
-        self.option1_900_button.clicked.connect(lambda:self.get_input("900"))
-        self.option1_main_button.clicked.connect(lambda:self.get_input("main"))
-        self.option1_basic_button.clicked.connect(lambda:self.get_input("basic"))
-        self.option1_else_button.clicked.connect(lambda:self.get_input("else"))
+        self.option1_800_button.clicked.connect(lambda:self.get_input(Category.EIGHT))
+        self.option1_900_button.clicked.connect(lambda:self.get_input(Category.NINE))
+        self.option1_main_button.clicked.connect(lambda:self.get_input(Category.MAIN))
+        self.option1_basic_button.clicked.connect(lambda:self.get_input(Category.BASIC))
+        self.option1_else_button.clicked.connect(lambda:self.get_input(Category.ELSE))
         
     #하하! 단어 좀 외워볼까!?
     def start_memorizing():
@@ -51,42 +71,32 @@ class MainClass(QMainWindow, main_class):
 
     #파이어베이스에 단어 저장
     def add_directly(self, eng, kor, category):
-        #파이어베이스 데이터베이스 접근
+        #파이어베이스 데이터베이스 접근 및 중복체크
         ref = db.reference()
+        new_kor = kor
+        if ref.child(category).child(eng).get() != None: #이미 있는 경우
+            old_kor = ref.child(category).child(eng).get()
+            new_kor = old_kor + '; ' + kor
+        else: #새롭게 추가하는 경우
+            old_numbering = ref.child('NUMBERING').child(category).get()
+            ref.child('NUMBERING').child(category).set(old_numbering + 1)
         
-        ref.child(category).child(eng).set(kor)
+        ref.child(category).child(eng).set(new_kor)
         
-        old_numbering = ref.child('numbering').child(category).get()
-        ref.child('numbering').child(category).set(old_numbering + 1)
         return
 
     #################### 중복 단어 체크 기능 필요!! 현재: 넘버링 추, 뜻 덮어쓰기.
     #단어 바로 등록하기!
     #category: STRING type
     def get_input(self, category):
-        arr = [[self.option1_800_button, self.option1_800_input_kor, self.option1_800_input_eng],
-               [self.option1_900_button, self.option1_900_input_kor, self.option1_900_input_eng],
-               [self.option1_basic_button, self.option1_basic_input_kor, self.option1_basic_input_eng],
-               [self.option1_main_button, self.option1_main_input_kor, self.option1_main_input_eng],
-               [self.option1_else_button, self.option1_else_input_kor, self.option1_else_input_eng]]
+        idx = category.value
 
-        idx = 0
-        if category == '900':
-            idx = 1
-        elif category == 'basic':
-            idx = 2
-        elif category == 'main':
-            idx = 3
-        elif category == 'else':
-            idx = 4
-        
         #get input
-        kor = arr[idx][1].toPlainText()
-        eng = arr[idx][2].toPlainText()
+        kor = self.arr[idx][1].toPlainText()
+        eng = self.arr[idx][2].toPlainText()
 
-        self.add_directly(eng, kor, category)
+        self.add_directly(eng, kor, category.name)
         return
-
 
     #파일로 단어 등록하기
     def add_file(file_name):
